40ca7ad847aac6aa2e75ff1d082fdaaf3e6e343d
Jeremy Ellis <keyfreemusic@gmail.com>
2021-04-24 13:41:12 -0700

    Update ISSUES-AND-NOTES.ino

--- libraries/LoRaRadio/examples/portenta-vision-shield/ISSUES-AND-NOTES/ISSUES-AND-NOTES.ino
+++ libraries/LoRaRadio/examples/portenta-vision-shield/ISSUES-AND-NOTES/ISSUES-AND-NOTES.ino
@@ -1,7 +1,14 @@
 /*
 *
 * To install core, just add a "hardware" folder beside your "libraries" folder in linux "$sketchbook" folder
+* on windows use documents/arduino/  and make a hardware folder.
 * Then unzip this repository in that "hardware" folder
+* Note: not all the code runs on windows but you can at least see the methods
+* Alternate install is to install https://github.com/GrumpyOldPizza/ArduinoCore-stm32l0 using the json file
+*
+* https://grumpyoldpizza.github.io/ArduinoCore-stm32l0/package_stm32l0_boards_index.json
+*
+* and then replace all those files with the files from this repository at https://github.com/hpssjellis/ArduinoCore-stm32l0
 *
 * For farthest LoRa sending max at 11 BYTES or 88 bits
 * Thats 11 letters perfect enough to send Machine Learning Coded information
