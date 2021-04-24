6fe8be82880da904ec7ae4c98deafb649441c689
Jeremy Ellis <keyfreemusic@gmail.com>
2021-04-24 13:32:12 -0700

    Create ISSUES-AND-NOTES.ino

---
+++ libraries/LoRaRadio/examples/portenta-vision-shield/ISSUES-AND-NOTES/ISSUES-AND-NOTES.ino
@@ -0,0 +1,25 @@
+/*
+*
+* For farthest LoRa sending max at 11 BYTES or 88 bits
+* Thats 11 letters perfect enough to send Machine Learning Coded information
+* or 88 bits if you want to Hexadecimal exchange data.  
+*
+* 1. I want to send data from a node and then wait for a confirmation
+* The confirmation can then effect the next data send
+*
+* 2. If no confirmation I want the node to try again but with
+* a stronger signal etc
+* If still no reply I want to connect to 
+* TTN (TheThingsNetwork) first if no connection then
+* Helium.org next
+*
+* Presntly working on getting a confirmation response.
+*
+*
+*
+*
+*
+*
+*
+*
+*/
