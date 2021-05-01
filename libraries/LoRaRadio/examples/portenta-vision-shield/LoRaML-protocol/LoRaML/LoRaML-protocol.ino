76dfb5f4a6dfcb8d1b877a8a59589fd969d62f0b
Jeremy Ellis <keyfreemusic@gmail.com>
2021-04-30 22:32:17 -0700

    Update and rename libraries/LoRaRadio/examples/portenta-vision-shield/LoRaML-protocol/LoRaML-protocol.ino to libraries/LoRaRadio/examples/portenta-vision-shield/LoRaML-protocol/LoRaML/LoRaML-protocol.ino

--- libraries/LoRaRadio/examples/portenta-vision-shield/LoRaML-protocol/LoRaML/LoRaML-protocol.ino
+++ libraries/LoRaRadio/examples/portenta-vision-shield/LoRaML-protocol/LoRaML/LoRaML-protocol.ino
@@ -16,7 +16,7 @@
 *          Showing fixed 10 Byte, Private, InvertIQ true (Gateway listening) nearby LoRa Activity. 
 *          Needs code that just broadcasts
 *
-* V-0.2.x  Above but showing on a WiFi webpage or (Ethernet if using the Portenta Breakout board)
+* V-0.2.x  Above but showing on a WiFi webpage or Adafruit.io MQTT or (Ethernet if using the Portenta Breakout board)
 *
 * V-0.3.x  Back to just serial display but from M7 core being able to send 10 bytes to Murata Module to broadcast as a Private, 
 *          InvertIQ false(Node send)
