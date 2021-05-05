b6cc7b938ebc1113bbcd16bb8f350cd4a4bcec00
Jeremy Ellis <keyfreemusic@gmail.com>
2021-05-05 12:43:07 -0700

    Create LoRaV3.ino

---
+++ libraries/LoRaRadio/examples/portenta-vision-shield/LoRaV3/LoRaV3.ino
@@ -0,0 +1,27 @@
+/*
+
+This will be a folder with sketches when they are ready to go.
+Presently just putting the .ino files in this folder
+
+
+Looking at multiple version of
+Gateway: receive from murata IQ true and send to Portenta
+     Send IQ false ----OK---- 
+     
+     advanced. Send confirmtion of Internet connectivity back to murata
+     
+     
+ Repeater: (danger if close to others)
+ Reads and re-sends IQ true traffic
+ Reads and re-sends IQ false traffic
+ that is all it does
+ 
+ 
+ Node: Murata Receives Portenta 10 characters
+ Sends 10 characters on IQ true
+ activates listening on IQ false
+ ...
+
+
+
+*/
